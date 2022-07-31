6ab6432eea40f4253af93630ff2794ec5878a9b1
Evgeny Kurnevsky <kurnevsky@gmail.com>
2022-07-31 11:28:12 +0300

    Downgrade bossa to 1.7.
    Higher versions don't work.

--- flake.nix
+++ flake.nix
@@ -16,7 +16,22 @@
           (callPackage ./openframeworks.nix { })
           (callPackage ./arduino-fwuploader.nix { })
           (callPackage ./firmware-uploader.nix { })
-          bossa
+          (bossa.overrideAttrs (old: rec {
+            version = "1.7.0";
+            src = fetchFromGitHub {
+              owner = "shumatech";
+              repo = "BOSSA";
+              rev = version;
+              sha256 = "sha256-yn8ZNyjny1JF8MwPtCtzUGeXWxM92rqrg5glSyQxMCE=";
+            };
+            patches = [
+              (fetchpatch {
+                url =
+                  "https://github.com/shumatech/BOSSA/commit/3ace00ee9a5a1372c321f4b97b941eb7c040ef80.patch";
+                sha256 = "sha256-o24IVj23KoUVwqPhX3i2Do2LkdMi951wRtDkwyXq9wc=";
+              })
+            ];
+          }))
         ];
       };
     };
